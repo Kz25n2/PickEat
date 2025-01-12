a8b1df58e486ba2b1e709ede8a1f93ebc2dc9b75
EC2 Default User <ec2-user@ip-172-31-46-192.eu-north-1.compute.internal>
2025-01-12 08:16:05 +0000

    会員側ログイン機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :restaurants
   devise_for :admins
-  devise_for :customers
+  
+  devise_for :customers, controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions",
+    passwords: "public/passwords"
+  }
+
+
   get 'homes/top'
   root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
